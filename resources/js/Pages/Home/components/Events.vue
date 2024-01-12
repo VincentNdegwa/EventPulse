b48ffa9fe6fa37185c7626d920602ef9bcc83a8c
VincentNdegwa <ndegwavincent7@gmail.com>
2024-01-12 16:10:13 +0300

    added some transitions

--- resources/js/Pages/Home/components/Events.vue
+++ resources/js/Pages/Home/components/Events.vue
@@ -42,163 +42,172 @@ export default {
         <div class="view-holder">
             <span>Latest Events</span>
             <div class="event-latest">
+
+
                 <div class="card-container" v-for="(item, index) in latestEvents" :key="index">
-                    <div class="card">
+                    <div class="card event_card">
                         <div class="card-image">
                             <img :src="item.event_image" alt="">
                         </div>
-                        <div class="event-date">{{ item.event_date }}</div>
-                        <div class="event-title">{{ item.title }}</div>
-                        <div class="event-description">{{ item.description }}</div>
-                        <div class="event-cta">View More&gt;</div>
+                        <div class="event_other_details">
+
+                            <div class="event-title">{{ item.title }}</div>
+                            <div class="event-description">{{ item.description }}</div>
+                            <div class="event-date">{{ item.event_date }}</div>
+                            <div class="event-cta"><i class='bx bxs-chevrons-right'></i></div>
+                        </div>
                     </div>
                 </div>
                 <div class="card-container">
-                    <div class="card">
+                    <div class="card event_card">
                         <div class="card-image">
                             <img src="images/hero.jpg" alt="">
                         </div>
-                        <div class="event-date">Wed/March 15 12:00pm</div>
-                        <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
-                        <div class="event-description">Explore the frontiers of innovation at our FutureTech
-                            Symposium,
-                            where
-                            visionaries converge to discuss emerging technologies</div>
-                        <div class="event-cta">View More&gt;</div>
-                    </div>
-                </div>
+                        <div class="event_other_details">
 
-                <div class="card-container">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="images/hero.jpg" alt="">
+                            <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
+                            <div class="event-description">Explore the frontiers of innovation at our FutureTech
+                                Symposium,
+                                where
+                                visionaries converge to discuss emerging technologies</div>
+                            <div class="event-date">Wed/March 15 12:00pm</div>
+                            <div class="event-cta"><i class='bx bxs-chevrons-right'></i></div>
                         </div>
-                        <div class="event-date">Wed/March 15 12:00pm</div>
-                        <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
-                        <div class="event-description">Explore the frontiers of innovation at our FutureTech
-                            Symposium,
-                            where
-                            visionaries converge to discuss emerging technologies</div>
-                        <div class="event-cta">View More&gt;</div>
-                    </div>
-                </div>
-
-
-
-
-                <div class="card-container">
-                    <div class="card">
-                        <div class="card-image">
-                            <img src="images/hero.jpg" alt="">
-                        </div>
-                        <div class="event-date">Wed/March 15 12:00pm</div>
-                        <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
-                        <div class="event-description">Explore the frontiers of innovation at our FutureTech
-                            Symposium,
-                            where
-                            visionaries converge to discuss emerging technologies</div>
-                        <div class="event-cta">View More&gt;</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="view-holder">
-            <span>Top Events</span>
-            <div class="event-latest">
-                <div class="card-container" v-for="(item, index) in trendingEvents" :key="index">
-                    <div class="card">
-                        <div class="card-image">
-                            <img :src="item.event_image" alt="">
-                        </div>
-                        <div class="event-date">{{ item.event_date }}</div>
-                        <div class="event-title">{{ item.title }}</div>
-                        <div class="event-description">{{ item.description }}</div>
-                        <div class="event-cta">View More&gt;</div>
                     </div>
                 </div>
                 <div class="card-container">
-                    <div class="card">
+                    <div class="card event_card">
                         <div class="card-image">
                             <img src="images/hero.jpg" alt="">
                         </div>
-                        <div class="event-date">Wed/March 15 12:00pm</div>
-                        <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
-                        <div class="event-description">Explore the frontiers of innovation at our FutureTech
-                            Symposium,
-                            where
-                            visionaries converge to discuss emerging technologies</div>
-                        <div class="event-cta">View More&gt;</div>
+                        <div class="event_other_details">
+
+                            <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
+                            <div class="event-description">Explore the frontiers of innovation at our FutureTech
+                                Symposium,
+                                where
+                                visionaries converge to discuss emerging technologies</div>
+                            <div class="event-date">Wed/March 15 12:00pm</div>
+                            <div class="event-cta"><i class='bx bxs-chevrons-right'></i></div>
+                        </div>
                     </div>
                 </div>
-
                 <div class="card-container">
-                    <div class="card">
+                    <div class="card event_card">
                         <div class="card-image">
                             <img src="images/hero.jpg" alt="">
                         </div>
-                        <div class="event-date">Wed/March 15 12:00pm</div>
-                        <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
-                        <div class="event-description">Explore the frontiers of innovation at our FutureTech
-                            Symposium,
-                            where
-                            visionaries converge to discuss emerging technologies</div>
-                        <div class="event-cta">View More&gt;</div>
+                        <div class="event_other_details">
+
+                            <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
+                            <div class="event-description">Explore the frontiers of innovation at our FutureTech
+                                Symposium,
+                                where
+                                visionaries converge to discuss emerging technologies</div>
+                            <div class="event-date">Wed/March 15 12:00pm</div>
+                            <div class="event-cta"><i class='bx bxs-chevrons-right'></i></div>
+                        </div>
                     </div>
                 </div>
-
                 <div class="card-container">
-                    <div class="card">
+                    <div class="card event_card">
                         <div class="card-image">
                             <img src="images/hero.jpg" alt="">
                         </div>
-                        <div class="event-date">Wed/March 15 12:00pm</div>
-                        <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
-                        <div class="event-description">Explore the frontiers of innovation at our FutureTech
-                            Symposium,
-                            where
-                            visionaries converge to discuss emerging technologies</div>
-                        <div class="event-cta">View More&gt;</div>
+                        <div class="event_other_details">
+
+                            <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
+                            <div class="event-description">Explore the frontiers of innovation at our FutureTech
+                                Symposium,
+                                where
+                                visionaries converge to discuss emerging technologies</div>
+                            <div class="event-date">Wed/March 15 12:00pm</div>
+                            <div class="event-cta"><i class='bx bxs-chevrons-right'></i></div>
+                        </div>
                     </div>
                 </div>
-
-
-
                 <div class="card-container">
-                    <div class="card">
+                    <div class="card event_card">
                         <div class="card-image">
                             <img src="images/hero.jpg" alt="">
                         </div>
-                        <div class="event-date">Wed/March 15 12:00pm</div>
-                        <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
-                        <div class="event-description">Explore the frontiers of innovation at our FutureTech
-                            Symposium,
-                            where
-                            visionaries converge to discuss emerging technologies</div>
-                        <div class="event-cta">View More&gt;</div>
+                        <div class="event_other_details">
+
+                            <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
+                            <div class="event-description">Explore the frontiers of innovation at our FutureTech
+                                Symposium,
+                                where
+                                visionaries converge to discuss emerging technologies</div>
+                            <div class="event-date">Wed/March 15 12:00pm</div>
+                            <div class="event-cta"><i class='bx bxs-chevrons-right'></i></div>
+                        </div>
                     </div>
                 </div>
-
-
-
                 <div class="card-container">
-                    <div class="card">
+                    <div class="card event_card">
                         <div class="card-image">
                             <img src="images/hero.jpg" alt="">
                         </div>
-                        <div class="event-date">Wed/March 15 12:00pm</div>
-                        <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
-                        <div class="event-description">Explore the frontiers of innovation at our FutureTech
-                            Symposium,
-                            where
-                            visionaries converge to discuss emerging technologies</div>
-                        <div class="event-cta">View More&gt;</div>
+                        <div class="event_other_details">
+
+                            <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
+                            <div class="event-description">Explore the frontiers of innovation at our FutureTech
+                                Symposium,
+                                where
+                                visionaries converge to discuss emerging technologies</div>
+                            <div class="event-date">Wed/March 15 12:00pm</div>
+                            <div class="event-cta"><i class='bx bxs-chevrons-right'></i></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-container">
+                    <div class="card event_card">
+                        <div class="card-image">
+                            <img src="images/hero.jpg" alt="">
+                        </div>
+                        <div class="event_other_details">
+
+                            <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
+                            <div class="event-description">Explore the frontiers of innovation at our FutureTech
+                                Symposium,
+                                where
+                                visionaries converge to discuss emerging technologies</div>
+                            <div class="event-date">Wed/March 15 12:00pm</div>
+                            <div class="event-cta"><i class='bx bxs-chevrons-right'></i></div>
+                        </div>
                     </div>
                 </div>
 
 
             </div>
-        </div>
+
+
+            <div class="view-holder">
+                <span>Top Events</span>
+                <div class="event-latest">
+
+
+
+                    <div class="card-container">
+                        <div class="card event_card">
+                            <div class="card-image">
+                                <img src="images/hero.jpg" alt="">
+                            </div>
+                            <div class="event_other_details">
+
+                                <div class="event-title">FutureTech Symposium: Shaping Tomorrow's World</div>
+                                <div class="event-description">Explore the frontiers of innovation at our FutureTech
+                                    Symposium,
+                                    where
+                                    visionaries converge to discuss emerging technologies</div>
+                                <div class="event-date">Wed/March 15 12:00pm</div>
+                                <div class="event-cta"><i class='bx bxs-chevrons-right'></i></div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
     </div>
-</template>
+</div></template>
