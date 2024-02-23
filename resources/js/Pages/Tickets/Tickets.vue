67a7f31e0a379af6c3164477e13a802f2c382892
VincentNdegwa <ndegwavincent7@gmail.com>
2024-02-23 21:21:13 +0300

    made some changes

--- resources/js/Pages/Tickets/Tickets.vue
+++ resources/js/Pages/Tickets/Tickets.vue
@@ -13,7 +13,6 @@ export default {
             mainTicketsData: [],
             eventData: {},
             ticketState: "pending"
-            // categories: []
         }
     },
     components: {
