86060cc33a10414cde85acc5634798d20e78d5d9
VincentNdegwa <ndegwavincent7@gmail.com>
2024-03-15 20:15:14 +0300

    changed the main ui

--- resources/js/Pages/Home/components/HeroSection.vue
+++ resources/js/Pages/Home/components/HeroSection.vue
@@ -4,8 +4,8 @@
             <div class="side-holder">
                 <h2>Crafting Unforgettable Moments Every Detail, Every Event, Every Time</h2>
                 <div class="button-holder">
-                    <button>create</button>
-                    <button>find event</button>
+                    <button>Create <i class='bx bxs-plus-square'></i></button>
+                    <button>Find Event <i class='bx bx-search-alt'></i></button>
                 </div>
             </div>
         </main>
