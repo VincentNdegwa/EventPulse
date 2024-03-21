2b834fbede7d3f4b0a8063910e88dc26f7556618
VincentNdegwa <ndegwavincent7@gmail.com>
2024-03-22 01:31:44 +0300

    winding up the contact

--- resources/js/Pages/Home/components/Contacts.vue
+++ resources/js/Pages/Home/components/Contacts.vue
@@ -25,8 +25,34 @@
             </div>
 
             <div class="contact_form row">
-                <form action="">
+                <form id="contactForm" class="container">
+                    <div class="row">
+                        <div class="mb-3 col">
+                            <label for="fullName" class="form-label">Full Name</label>
+                            <input type="text" class="form-control" id="fullName" name="fullName" required>
+                        </div>
+                        <div class="mb-3 col">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+                    </div>
 
+                    <div class="mb-3">
+                        <label for="helpOption" class="form-label">How can we help you?</label>
+                        <select class="form-select" id="helpOption" name="helpOption" required>
+                            <option value="">Select an option</option>
+                            <option value="General Inquiry">General Inquiry</option>
+                            <option value="Technical Support">Technical Support</option>
+                            <option value="Billing Issue">Billing Issue</option>
+                            <option value="Feedback">Feedback</option>
+                            <option value="Other">Other</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="message" class="form-label">Message</label>
+                        <textarea class="form-control" id="message" name="message" rows="5" required></textarea>
+                    </div>
+                    <button type="submit" class="btn submit_btn">Submit</button>
                 </form>
             </div>
         </div>
@@ -35,14 +61,18 @@
 </template>
 
 <style>
+.submit_btn {
+    background: var(--main-blue);
+    color: var(--main-white) !important;
+    border: 1px solid var(--main-blue) !important;
+}
+
 .contact_container {
     width: 100%;
-    height: 90vh;
+    height: auto;
     background: var(--main-green);
-
-    &>.container {
-        border: 1px solid;
-    }
+    margin-bottom: 20px;
+    padding: 20px;
 }
 
 .details {
@@ -89,5 +119,14 @@
 
 img {
     width: 300px;
+}
+
+.contact_form {
+    margin-top: 3rem;
+    background: rgb(255, 255, 255, 0.3);
+    backdrop-filter: blur(20px);
+    border-radius: 1rem;
+    padding: 1rem;
+    box-shadow: 0 8px 32px 0 var(--main-blue);
 }
 </style>
