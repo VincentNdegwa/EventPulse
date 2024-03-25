c16db2e21d5710073ffc4a2afedd1022e909d5a8
VincentNdegwa <ndegwavincent7@gmail.com>
2024-03-25 23:44:56 +0300

    made s slight change

--- resources/js/Pages/Home/components/AboutUs.vue
+++ resources/js/Pages/Home/components/AboutUs.vue
@@ -1,111 +1,151 @@
 <template>
     <div class="about_container p-4">
-        <h3 class="text-center fs-1">Who Are We?</h3>
-        <p class="mb-2 text-center">We are Joy Bringers, we believe that every event has the potential to be
-            extraordinary. As a leading
-            provider of event management solutions, we are dedicated to empowering individuals and organizations to
-            create memorable and impactful experiences that bring communities together. With our user-friendly platform,
-            you can effortlessly plan, promote, and manage events of all sizes, from intimate gatherings to large-scale
-            conferences and festivals.
-        </p>
-        <div class="details_container mt-10">
+        <div class="background">
+            <img src="images/background3D.jpg" alt="background">
+        </div>
+        <div class="about_holder">
+
+            <h3 class="text-center fs-1">Who Are We?</h3>
+            <p class="mb-2 text-center">We are Joy Bringers, we believe that every event has the potential to be
+                extraordinary. As a leading
+                provider of event management solutions, we are dedicated to empowering individuals and organizations to
+                create memorable and impactful experiences that bring communities together. With our user-friendly
+                platform,
+                you can effortlessly plan, promote, and manage events of all sizes, from intimate gatherings to
+                large-scale
+                conferences and festivals.
+            </p>
+            <div class="details_container mt-10">
 
 
-            <div class="details_item">
-                <div class="line">
-                    <span>
-                        <i class='bx bx-wifi-0'></i>
-                    </span>
-                    <p>
+                <div class="details_item">
+                    <div class="line">
+                        <span>
+                            <i class='bx bx-wifi-0'></i>
+                        </span>
+                        <p>
 
-                    </p>
-                </div>
-                <div class="content">
-                    <div class="fs-4 text-center">Our Mission</div>
-                    <div class="more_content_container">
-                        <div class="fs-6 content_text">
-                            Our mission is to democratize event planning by providing accessible tools and resources
-                            that enable anyone to bring their vision to life. We strive to simplify the event management
-                            process, streamline communication between organizers and attendees, and foster meaningful
-                            connections that leave a lasting impression.
-                        </div>
-                        <div class="illustator_holder">
-                            <img src="images/mission.png" alt="mission">
-                        </div>
+                        </p>
                     </div>
-                </div>
-            </div>
-
-
-            <!-- end -->
-
-            <div class="details_item opp">
-
-                <div class="content">
-                    <div class="fs-4 text-center">Our Vision</div>
-                    <div class="more_content_container">
-                        <div class="fs-6 content_text">
-                            Our vision is to be the premier destination for event creators and attendees alike, offering
-                            a seamless and immersive experience that transcends traditional event planning boundaries.
-                            We envision a world where events are not just occasions but transformative experiences that
-                            inspire, educate, and entertain.
-                        </div>
-                        <div class="illustator_holder">
-                            <img src="images/vision.png" alt="mission">
+                    <div class="content">
+                        <div class="fs-4 text-center">Our Mission</div>
+                        <div class="more_content_container">
+                            <div class="fs-6 content_text">
+                                Our mission is to democratize event planning by providing accessible tools and resources
+                                that enable anyone to bring their vision to life. We strive to simplify the event
+                                management
+                                process, streamline communication between organizers and attendees, and foster
+                                meaningful
+                                connections that leave a lasting impression.
+                            </div>
+                            <div class="illustator_holder">
+                                <img src="images/mission.png" alt="mission">
+                            </div>
                         </div>
                     </div>
                 </div>
 
-                <div class="line">
-                    <span>
-                        <i class='bx bx-wifi-0'></i>
-                    </span>
-                    <p>
 
-                    </p>
-                </div>
-            </div>
+                <!-- end -->
 
+                <div class="details_item opp">
 
-            <!-- end -->
-
-            <div class="details_item">
-                <div class="line">
-                    <span>
-                        <i class='bx bx-wifi-0'></i>
-                    </span>
-                    <p>
-
-                    </p>
-                </div>
-                <div class="content">
-                    <div class="fs-4 text-center">Goals</div>
-                    <div class="more_content_container">
-                        <div class="fs-6 content_text">
-                            <ul>
-                                <li>Empowerment: Provide event organizers with tools and support for successful
-                                    planning.</li>
-                                <li>Accessibility: Make event planning intuitive and affordable for all.</li>
-                                <li>Community Building: Foster an inclusive community for idea sharing and
-                                    collaboration.</li>
-                                <li>Innovation: Drive industry innovation with technology and creative solutions.</li>
-                                <li>Impact: Create events that promote diversity, sustainability, and social
-                                    responsibility.</li>
-                            </ul>
+                    <div class="content">
+                        <div class="fs-4 text-center">Our Vision</div>
+                        <div class="more_content_container">
+                            <div class="fs-6 content_text">
+                                Our vision is to be the premier destination for event creators and attendees alike,
+                                offering
+                                a seamless and immersive experience that transcends traditional event planning
+                                boundaries.
+                                We envision a world where events are not just occasions but transformative experiences
+                                that
+                                inspire, educate, and entertain.
+                            </div>
+                            <div class="illustator_holder">
+                                <img src="images/vision.png" alt="mission">
+                            </div>
                         </div>
-                        <div class="illustator_holder">
-                            <img src="images/goal.png" alt="mission">
+                    </div>
+
+                    <div class="line">
+                        <span>
+                            <i class='bx bx-wifi-0'></i>
+                        </span>
+                        <p>
+
+                        </p>
+                    </div>
+                </div>
+
+
+                <!-- end -->
+
+                <div class="details_item">
+                    <div class="line">
+                        <span>
+                            <i class='bx bx-wifi-0'></i>
+                        </span>
+                        <p>
+
+                        </p>
+                    </div>
+                    <div class="content">
+                        <div class="fs-4 text-center">Goals</div>
+                        <div class="more_content_container">
+                            <div class="fs-6 content_text">
+                                <ul>
+                                    <li>Empowerment: Provide event organizers with tools and support for successful
+                                        planning.</li>
+                                    <li>Accessibility: Make event planning intuitive and affordable for all.</li>
+                                    <li>Community Building: Foster an inclusive community for idea sharing and
+                                        collaboration.</li>
+                                    <li>Innovation: Drive industry innovation with technology and creative solutions.
+                                    </li>
+                                    <li>Impact: Create events that promote diversity, sustainability, and social
+                                        responsibility.</li>
+                                </ul>
+                            </div>
+                            <div class="illustator_holder">
+                                <img src="images/goal.png" alt="mission">
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
 
             <!-- end -->
         </div>
     </div>
 </template>
 <style>
+.about_container {
+    position: relative;
+
+}
+
+.about_holder {
+    z-index: 1;
+    position: relative;
+}
+
+.background {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 0;
+
+    &>img {
+        object-fit: cover;
+        width: 100%;
+        height: 100%;
+        object-position: center;
+    }
+}
+
 .details_container {
     display: flex;
     flex-direction: column;
--- resources/js/Pages/Home/components/Footer.vue
+++ resources/js/Pages/Home/components/Footer.vue
@@ -1,5 +1,12 @@
 <template>
    <footer>
-    
+
    </footer>
 </template>
+<style>
+footer {
+   width: 100%;
+   height: 30vh;
+   
+}
+</style>
